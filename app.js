3ce42b162421b789500a405202e2fefe4231c7bd
Titouan Goubet <titouangoubet@hotmail.fr>
2024-10-02 19:56:20 +0200

    Connexion a la base mysql avec sequelize

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { ConnectToDatabase, connectToDatabase } = require("./config/database");
 
 // Middleware pour pouvoir analyser les format JSON
 // En gros on peut parser les requete http contenant du JSON dans le body
 app.use(express.json());
+
+// Connexion a notre bddmysql
+connectToDatabase();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
