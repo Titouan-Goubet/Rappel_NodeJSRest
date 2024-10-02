ba867d9cc5b44daabd799354d98adde27806c85c
Titouan Goubet <titouangoubet@hotmail.fr>
2024-10-02 19:36:05 +0200

    Initialisation de l'api nodejs, express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware pour pouvoir analyser les format JSON
+// En gros on peut parser les requete http contenant du JSON dans le body
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Serveur démarré sur http://localhost:${port}`);
+});
