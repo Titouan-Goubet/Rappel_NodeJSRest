9d1bbf084917f982995edbe2b710588691435317
Titouan Goubet <titouangoubet@hotmail.fr>
2024-10-03 17:16:55 +0200

    Retrait de sequelize pour prisma, meilleur compatibiltié avec TS

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
-const { connectToDatabase } = require("./config/database");
 
 // Middleware pour pouvoir analyser les format JSON
 // En gros on peut parser les requete http contenant du JSON dans le body
 app.use(express.json());
-
-// Connexion a notre bddmysql
-connectToDatabase();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
