2c323f71f567ffa449f75920c9330f189ee156e2
Titouan Goubet <titouangoubet@hotmail.fr>
2024-10-07 20:53:37 +0200

    Ajout de rate limiter et helmet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 import { errorHandler } from "./middlewares/errorHandler";
 import authRoutes from "./routes/authRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
@@ -7,11 +9,20 @@ import productRoutes from "./routes/productRoutes";
 const app = express();
 const port = 3000;
 
+// Rate limiter pour empécher un éventuel bruteforce d'un utilisateur malveillant
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 const corsOptions = {
   origin: "http://localhost:5173", // Autorisation de mon futur front end React
   optionsSuccessStatus: 200,
 };
 
+app.disable("x-powered-by");
+app.use(limiter);
+app.use(helmet());
 app.use(cors(corsOptions));
 app.use(express.json());
 
