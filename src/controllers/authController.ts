5517c523012560973e1f0f0322c41a83713fdd1f
Titouan Goubet <titouangoubet@hotmail.fr>
2024-10-07 20:23:24 +0200

    Ajout d'une authentification JWT avec token de rafriachissement

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -33,13 +33,18 @@ export const register = asyncHandler(async (req: Request, res: Response) => {
 export const login = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
+  // Verif email existe
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user) {
-    return res
-      .status(400)
-      .json({ message: "Email ou mot de passe incorrect." });
+    return (
+      res
+        .status(400)
+        // On reste évasif pour ne pas se faire bruteforce les email
+        .json({ message: "Email ou mot de passe incorrect." })
+    );
   }
 
+  // Verif mot de passe
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
     return res
@@ -47,11 +52,82 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
       .json({ message: "Email ou mot de passe incorrect." });
   }
 
-  const token = jwt.sign(
+  const accessToken = jwt.sign(
     { userId: user.id, email: user.email },
     process.env.JWT_SECRET as string,
-    { expiresIn: "1h" }
+    { expiresIn: "15m" }
   );
 
-  res.json({ token });
+  const refreshToken = jwt.sign(
+    { userId: user.id },
+    process.env.JWT_REFRESH_SECRET as string,
+    { expiresIn: "7d" }
+  );
+
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { refreshToken },
+  });
+
+  // Le refresh token part en cookie http only et sera utilisé uniquement pour refresh l'access token, il ne sert pas a authentifier
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
+  });
+
+  res.json({ accessToken });
+});
+
+// L'utilisateur doit refresh son access token tous les 15 mn par soucis de sécurité
+export const refreshToken = asyncHandler(
+  async (req: Request, res: Response) => {
+    // On récupére alors le refreshToken dans les cookie http only et on vérifie sa validité
+    const { refreshToken } = req.cookies;
+
+    // Les cookies HTTP-only sont automatiquement envoyés par le navigateur avec chaque requête HTTP au domaine auquel ils appartiennent.
+
+    if (!refreshToken) {
+      return res.status(401).json({ message: "Refresh token manquant." });
+    }
+
+    try {
+      const payload = jwt.verify(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string
+      ) as { userId: string };
+      const user = await prisma.user.findUnique({
+        where: { id: payload.userId },
+      });
+
+      if (!user || user.refreshToken !== refreshToken) {
+        return res.status(401).json({ message: "Refresh token invalide." });
+      }
+
+      const newAccessToken = jwt.sign(
+        { userId: user.id, email: user.email },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "15m" }
+      );
+
+      res.json({ accessToken: newAccessToken });
+    } catch (error) {
+      res.status(401).json({ message: "Refresh token invalide." });
+    }
+  }
+);
+
+export const logout = asyncHandler(async (req: Request, res: Response) => {
+  const { refreshToken } = req.cookies;
+
+  if (refreshToken) {
+    await prisma.user.updateMany({
+      where: { refreshToken },
+      data: { refreshToken: null },
+    });
+  }
+
+  res.clearCookie("refreshToken");
+  res.json({ message: "Déconnexion réussie." });
 });
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { login, register } from "../controllers/authController";
+import {
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/authController";
 import { validateLogin, validateRegister } from "../middlewares/validation";
 
 const router = express.Router();
 
 router.post("/register", validateRegister, register);
 router.post("/login", validateLogin, login);
+router.post("/refresh-token", refreshToken);
+router.post("/logout", logout);
 
 export default router;
